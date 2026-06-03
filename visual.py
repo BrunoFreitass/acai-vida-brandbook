2a02098ff596407d15943a70bbccbe720951da8e
BrunoFreitass <63013286+BrunoFreitass@users.noreply.github.com>
2026-06-03 14:36:31 -0400

    Update visual.py

--- visual.py
+++ visual.py
@@ -291,7 +291,7 @@ with col2:
         st.image("sorvete_01.png", use_container_width=True)
     if Path("sorvete_02.png").exists():
         st.image("sorvete_02.png", use_container_width=True)
-    if Path("sorvertes_fundo_amarelo_01.png").exists():
+    if Path("sovertes_fundo_amarelo_01.png").exists():
         st.image("sovertes_fundo_amarelo_01.png", use_container_width=True)
 
 st.markdown("---")
